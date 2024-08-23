d7ac3b41d275940aa40694d464d5d91a0fbeac7c
Revathy <revathymr916@gmail.com>
2024-08-23 16:27:17 +0530

    Login and Signup page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import Advertiser from './pages/Advertiser'
-import Publisher from './pages/Publisher'
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Advertiser from "./pages/Advertiser";
+import Publisher from "./pages/Publisher";
+import LoginAndSignup from "./pages/LoginAndSignup/LoginAndSignup";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="w-screen flex justify-center bg-black">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<LoginAndSignup />} />
           <Route path="/advertiser/*" element={<Advertiser />} />
           <Route path="/publisher/*" element={<Publisher />} />
         </Routes>
       </BrowserRouter>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
